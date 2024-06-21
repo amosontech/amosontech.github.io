76ee3fad0e0cc97c8214c75e71db3959cfb17aa7
amosontech <melchizedekamos20@gmail.com>
2024-06-21 16:47:54 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+'use strict';
+
+/**
+ *  add event listener on multiple elements
+*/
+
+const addEventOnElements = function(elements, eventType, callback){
+    for(let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+/**
+ * MOBILE NAVBAR TOGGLER
+*/
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navTogglersBtn = document.querySelectorAll("[data-nav-toggler-btn]");
+
+const toggleNav = ()=>{
+    navbar.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglersBtn, "click", toggleNav);
+
+addEventOnElements(navTogglers, "click", function(){
+    const element = document.querySelector(".nav-open-btn");
+    const cssObj = window.getComputedStyle(element, null);
+
+    var display = cssObj.getPropertyValue("display");
+    
+    if (display == "none") {
+    }else{
+        toggleNav();
+    }
+});
+
+/**
+ * HEADER ANIMATION
+ * When scrolled down to 100px header will be active
+*/
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+window.addEventListener("scroll", ()=>{
+    if (window.scrollY > 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    }else{
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+});
+
+/**
+ * SLIDER
+*/
+
+const slider = document.querySelector("[data-slider]");
+const sliderContainer = document.querySelector("[data-slider-contaner]");
+const sliderPrevBtn = document.querySelector("[data-slider-prev]");
+const sliderNextBtn = document.querySelector("[data-slider-next]");
+
+let totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlidePos = 0;
+
+const moveSlideItem = function(){
+    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+}
+
+/**
+ * NEXT SLIDE
+*/
+
+const slideNext = function(){
+    const slideEnd = currentSlidePos >= totalSlidableItems;
+
+    if(slideEnd){
+        currentSlidePos = 0;
+    }else{
+        currentSlidePos++;
+    }
+    moveSlideItem();
+}
+
+sliderNextBtn.addEventListener("click", slideNext);
+
+/**
+ * PREV SLIDE
+*/
+
+const slidePrev = function(){
+    if(currentSlidePos <= 0){
+        currentSlidePos = totalSlidableItems;
+    }else{
+        currentSlidePos--;
+    }
+    moveSlideItem();
+}
+
+sliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * RESPONSIVE
+*/
+
+window.addEventListener("resize", function (){
+    totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+    totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+    moveSlideItem()
+});
